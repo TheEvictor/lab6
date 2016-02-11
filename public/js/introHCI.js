777545cf8dc30ba0f66573b0a423715141861bdc
Austin Chinn <achinn@ucsd.edu>
2016-02-10 22:43:27 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,7 +26,20 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	$.get("/project/" + idNumber, projectClick);
+
 	console.log("User clicked on project " + idNumber);
+}
+
+function projectClick(result) {
+	console.log(result);
+
+	var newHTML = '<a href="#" class="thumbnail">' + 
+	'<img src="' + result['image'] + '"class="img">' +
+	'<p>' + result['title'] + '</p>' +
+	'<p><small>' + result['date'] + '</small></p></a>' +
+	'<p>' + result['summary'] + '</p>'; 
+	$("#project" + result['id'] + " .details").html(newHTML);
 }
 
 /*
@@ -35,4 +48,19 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	$.get("/palette", colorClick);
+}
+
+function colorClick(result) {
+	console.log(result);
+
+	var color = result['colors'];
+	var colors = color.hex;
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
